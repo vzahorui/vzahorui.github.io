fa5268331a26fa05b74e88e0d213720f4aefc9cc
Vladyslav Zahorui <vzahorui.dev@gmail.com>
2025-08-18 14:21:17 +0300

    adjusted graph height

--- assets/js/graph.js
+++ assets/js/graph.js
@@ -1,7 +1,7 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7/+esm';
 
 const WIDTH = 1800;
-const HEIGHT = 2200;
+const HEIGHT = 1800;
 
 const raw = document.getElementById('graph-data').textContent;
 const graph = JSON.parse(raw);
